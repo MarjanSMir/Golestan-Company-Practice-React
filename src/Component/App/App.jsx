43082fade36b086dd0674ec226a222f80adc2cd3
MarjanSmir <Marjansmirmohammad@outlook.com>
2024-09-17 11:29:19 +0330

    Footer

--- src/Component/App/App.jsx
+++ src/Component/App/App.jsx
@@ -1,11 +1,13 @@
 import Header from "../Header/Header";
 import Body from "../Body/Body";
+import Footer from "../Footer/Footer"
 import "./App.css"
 let App = () =>{
     return(
         <div>
             <Header />
             <Body />
+            <Footer />
         </div>
     )
 }
--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,11 +1,81 @@
 import "./footer.css";
 const Footer = () => {
   return (
-    <>
-    
-  
-  
-    </>
+        <div className="footer">
+            <div className="social-media">
+                <div className="round gl">
+                    <link rel="stylesheet" href="" />
+                </div>
+                <div className="round facebook">
+                    <link rel="stylesheet" href="" />
+                </div>
+                <div className="round aparat">
+                    <link rel="stylesheet" href="" />
+                </div>
+                <div className="round instagram">
+                    <link rel="stylesheet" href="" />
+                </div>
+                <div className="round telegram">
+                    <link rel="stylesheet" href="" />
+                </div>
+                <div className="round linkedin">
+                    <link rel="stylesheet" href="" />
+                </div>
+            </div>
+            <div className="red-bar"></div>
+            <div className="footer-menu">
+                <div className="column">
+                    <div className="image"></div>
+                    <div className="title-box"></div>
+                    <ul>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                </div>
+                <div className="column">
+                    <div className="image"></div>
+                    <div className="title-box"></div>
+                    <ul>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                </div>
+                <div className="column">
+                    <div className="image"></div>
+                    <div className="title-box"></div>
+                    <ul>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                </div>
+                <div className="column">
+                    <div className="image"></div>
+                    <div className="title-box"></div>
+                    <ul>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                </div>
+            </div>
+            <span className="copyright"></span>
+            <span className="copyright"></span>
+        </div>
   )
 
 };
